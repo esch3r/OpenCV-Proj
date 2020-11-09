0e0f0999757e027c42bb97d23a471920d399b237
Johnathan Machler <machl005@umn.edu>
2020-11-09 00:37:33 -0600

    Add files via upload

---
+++ ColourHistogram.py
@@ -0,0 +1,17 @@
+# import packages
+import imutils
+import cv2
+B
+B
+B
+class RGBHistogram:
+     def __init__ (self,bins)
+         self.bins = bins
+         
+     def describe (self,image    )
+         hist = cv2.calcHist  ([image],[0,1,2],None, self.bins,[0,256,0,256,0,256]))
+         if imutils.is_cv2():
+            hist = cv2.normalize(histi)
+        else: 
+            hist = cv2.normalize((hist,hist))
+        return hist flatten() 
